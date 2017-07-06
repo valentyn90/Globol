fdee52b36542daf27a94528842167f01c05dd8aa
itsumrat <itsumrat@gmail.com>
2017-07-06 21:18:02 +0600

    navigation

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -42,6 +42,20 @@ jQuery(document).ready(function($){
 	    } // End if
 	});
 
+    // Window Scrolling for navbar
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $(".navbar").css("display", "none");
+            $(".scrolling-nav").css("display", "block");
+        }
+
+        else{
+            $(".navbar").css("display" , "flex");   
+            $(".scrolling-nav").css("display", "none");
+        }
+    })
+
 });
 
 
@@ -317,79 +331,6 @@ jQuery(document).ready(function($){
 })(jQuery);            
 
 
-// 3d slider
-/*var Slider = (function() {
-    var initSlider = function() {
-        var dir = $("html").attr("dir");
-        var swipeHandler = new Hammer(document.getElementById("slider"));
-        swipeHandler.on('swipeleft', function(e) {
-            if (dir == "rtl")
-                $(".arrow-left").trigger("click");
-            else
-                $(".arrow-right").trigger("click");
-        });
-
-        swipeHandler.on('swiperight', function(e) {
-            if (dir == "rtl")
-                $(".arrow-right").trigger("click");
-            else
-                $(".arrow-left").trigger("click");
-        });
-
-        $(".arrow-right , .arrow-left").click(function(event) {
-            var nextActiveSlide = $(".slide.active").next();
-
-            if ($(this).hasClass("arrow-left"))
-                nextActiveSlide = $(".slide.active").prev();
-
-            if (nextActiveSlide.length > 0) {
-                var nextActiveIndex = nextActiveSlide.index();
-                $(".dots span").removeClass("active");
-                $($(".dots").children()[nextActiveIndex]).addClass("active");
-
-                updateSlides(nextActiveSlide);
-            }
-        });
-
-        $(".dots span").click(function(event) {
-            var slideIndex = $(this).index();
-            var nextActiveSlide = $($(".slider").children()[slideIndex]);
-            $(".dots span").removeClass("active");
-            $(this).addClass("active");
-
-            updateSlides(nextActiveSlide);
-        });
-
-        var updateSlides = function(nextActiveSlide) {
-            var nextActiveSlideIndex = $(nextActiveSlide).index();
-
-            $(".slide").removeClass("prev-1");
-            $(".slide").removeClass("next-1");
-            $(".slide").removeClass("active");
-            $(".slide").removeClass("prev-2");
-            $(".slide").removeClass("next-2");
-
-            nextActiveSlide.addClass("active");
-
-            nextActiveSlide.prev().addClass("prev-1");
-            nextActiveSlide.prev().prev().addClass("prev-2");
-            nextActiveSlide.addClass("active");
-            nextActiveSlide.next().addClass("next-1");
-            nextActiveSlide.next().next().addClass("next-2");
-        }
-
-        var updateToNextSlide = function(nextActiveSlide)
-        {
-            
-        }
-    }
-    return {
-        init: function() {
-            initSlider();
-        }
-    }
-})();*/
-
 $(document).on('ready', function() {
   var slide = $('.slider-single');
   var slideTotal = slide.length - 1;
@@ -477,3 +418,7 @@ $(document).on('ready', function() {
   });
   slideInitial();
 });
+
+
+
+
