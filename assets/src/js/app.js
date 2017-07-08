f21e7b888e26473f8ef14b22db8ee265f8495912
itsumrat <itsumrat@gmail.com>
2017-07-09 03:18:55 +0600

    added video

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -18,6 +18,11 @@ jQuery(document).ready(function($){
 	  allowClear: true
 	});
 
+    // mx-auto class remove
+    if($(this).width() < 554) {
+        $('.navbar-brand').removeClass('mx-auto');
+    }
+
 	// Scroll to down
 	// Add smooth scrolling to all links
 	$("#explore").on('click', function(event) {
@@ -45,7 +50,8 @@ jQuery(document).ready(function($){
     // Window Scrolling for navbar
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {
+        var windowWidth = $(window).width();
+        if ((scroll > 300) && (windowWidth > 554)) {
             $(".navbar").css("display", "none");
             $(".scrolling-nav").css("display", "block");
         }
@@ -331,93 +337,6 @@ jQuery(document).ready(function($){
 })(jQuery);            
 
 
-/*$(document).on('ready', function() {
-  var slide = $('.slider-single');
-  var slideTotal = slide.length - 1;
-  var slideCurrent = -1;
-
-  function slideInitial() {
-    slide.addClass('proactivede');
-    setTimeout(function() {
-      slideRight();
-    }, 500);
-  }
-
-  function slideRight() {
-    if (slideCurrent < slideTotal) {
-      slideCurrent++;
-    } else {
-      slideCurrent = 0;
-    }
-
-    if (slideCurrent > 0) {
-      var preactiveSlide = slide.eq(slideCurrent - 1);
-    } else {
-      var preactiveSlide = slide.eq(slideTotal);
-    }
-    var activeSlide = slide.eq(slideCurrent);
-    if (slideCurrent < slideTotal) {
-      var proactiveSlide = slide.eq(slideCurrent + 1);
-    } else {
-      var proactiveSlide = slide.eq(0);
-
-    }
-
-    slide.each(function() {
-      var thisSlide = $(this);
-      if (thisSlide.hasClass('preactivede')) {
-        thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
-      }
-      if (thisSlide.hasClass('preactive')) {
-        thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
-      }
-    });
-    preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
-    activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
-    proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
-  }
-
-  function slideLeft() {
-    if (slideCurrent > 0) {
-      slideCurrent--;
-    } else {
-      slideCurrent = slideTotal;
-    }
-
-    if (slideCurrent < slideTotal) {
-      var proactiveSlide = slide.eq(slideCurrent + 1);
-    } else {
-      var proactiveSlide = slide.eq(0);
-    }
-    var activeSlide = slide.eq(slideCurrent);
-    if (slideCurrent > 0) {
-      var preactiveSlide = slide.eq(slideCurrent - 1);
-    } else {
-      var preactiveSlide = slide.eq(slideTotal);
-    }
-    slide.each(function() {
-      var thisSlide = $(this);
-      if (thisSlide.hasClass('proactivede')) {
-        thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
-      }
-      if (thisSlide.hasClass('proactive')) {
-        thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
-      }
-    });
-    preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
-    activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
-    proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
-  }
-  var left = $('.slider-left');
-  var right = $('.slider-right');
-  left.on('click', function() {
-    slideLeft();
-  });
-  right.on('click', function() {
-    slideRight();
-  });
-  slideInitial();
-});*/
 
 
 
